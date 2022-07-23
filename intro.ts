edee2a86929d7b96dccc5a8750c63542a1bfdca2
Eevikettu <kettunen.eevi@gmail.com>
2022-07-23 23:20:56 +0200

    intro notes

---
+++ intro.ts
@@ -0,0 +1,5 @@
+//Dymamic vs Static typing
+
+// in static ttyping you always predifine the type like string, boolean or number
+
+// typescript files will have ts or tsx which is like jsx but in typescript
