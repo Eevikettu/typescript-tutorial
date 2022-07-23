c26b533dd0dfdd7545ea46c9a51fde4025e89136
christopher <christopher@infinitelambda.com>
2022-07-23 23:26:11 +0200

    example

--- intro.ts
+++ intro.ts
@@ -3,3 +3,6 @@
 // in static ttyping you always predifine the type like string, boolean or number
 
 // typescript files will have ts or tsx which is like jsx but in typescript
+
+
+// theuteohutoehueoue
